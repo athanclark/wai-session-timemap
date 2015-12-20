955db9a629c56ab54ce76913d52154fefcaac249
Athan Clark <athan.clark@gmail.com>
2015-12-20 15:21:29 -0700

    updated latest version of sessions

--- src/Network/Wai/Session/TimeMap.hs
+++ src/Network/Wai/Session/TimeMap.hs
@@ -10,7 +10,6 @@ import Data.UUID
 import Data.UUID.V4   (nextRandom)
 import Data.Hashable
 import qualified Data.TimeMap           as TM
-import qualified Data.Vault.Lazy        as V
 import qualified Data.ByteString        as BS
 
 import Control.Concurrent.STM
@@ -22,10 +21,9 @@ uuidSessionCfg :: ( Hashable k
                     -> (BS.ByteString -> Maybe k) -- ^ parse key
                     -> BS.ByteString              -- ^ cookie name for key
                     -> BS.ByteString              -- ^ cookie name for UUID
-                    -> V.Key k                    -- ^ vault variable to store successful session lookups
                     -> TM.TimeMap k UUID          -- ^ session cache reference
                     -> SessionConfig IO k UUID
-uuidSessionCfg rKey pKey keyN valN vKey cache =
+uuidSessionCfg rKey pKey keyN valN cache =
   SessionConfig
     { renderKey = rKey
     , renderVal = toASCIIBytes
@@ -35,7 +33,6 @@ uuidSessionCfg rKey pKey keyN valN vKey cache =
     , valName   = valN
     , expire    = 3600
     , newVal    = updateSession
-    , vaultVar  = vKey
     }
   where
     updateSession sid nonce = do
